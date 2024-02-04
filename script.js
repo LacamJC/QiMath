a947075af2baed207b6385b255615d5daaed1a5d
LacamJC <joaoramajo744@gmail.com>
2024-02-03 21:12:56 -0300

    Adicionando comentarios

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Instanciando objetos da pagina
 const boxN1 = document.getElementById('n1')
 const boxN2 = document.getElementById('n2')
 const boxRes = document.getElementById('resultado')
@@ -7,17 +8,18 @@ const btn = document.getElementById('btn')
 const aviso = document.getElementById('aviso')
 const pular = document.getElementById('pular')
 
-pular.addEventListener('click', ()=>{
-    location.reload()
+
+pular.addEventListener('click', ()=>{ 
+    location.reload()// Atualizando a pagina caso o usuario clique em reiniciar
 })
 
-var n1 = Math.floor(Math.random() * 50);
-var n2 = Math.floor(Math.random() * 50);
-var ctl = Math.floor(Math.random() * 3);
+var n1 = Math.floor(Math.random() * 50); // Gerando o valor aleatorio entre 0 e 50, se quiser aumentar apenas mude o valor 50 para o desejado
+var n2 = Math.floor(Math.random() * 50); 
+var ctl = Math.floor(Math.random() * 3); // Aqui um numero random ira decidir o operador da conta 
 var _resultado = 0;
-var operador = "";
+var operador = "";// Esse aqui é somente para questoes visuais na pagina html
 
-switch(ctl)
+switch(ctl) // Switch para controlar resultado e conta
 {
     case 0:
         _resultado = n1 + n2 
@@ -32,16 +34,16 @@ switch(ctl)
                 operador = "x"
                 break
                 case 3:
-                    _resultado = (n1 / n2).toFixed(2) 
+                    _resultado = (n1 / n2).toFixed(2) // Talvez as funçoes de divisão estejam meio erradas
                     operador = "/"
                     break;
 
 }
 
-var random = Math.floor(Math.random() * 2)
+var random = Math.floor(Math.random() * 2) // Aqui gera um valor para escolher qual sera o numero escondido
 
 console.log(random)
-switch(random)
+switch(random)// Switch para esconder o operador
 {
     case 0:
         boxN1.innerHTML = "?"
@@ -59,10 +61,7 @@ boxOp.innerHTML = operador
 
 
 
-btn.addEventListener('click', ()=>{
-    // console.log("Resposta")
-    // console.log(resposta.value)
-
+btn.addEventListener('click', ()=>{ // Um evento click no botao resultado para verificar se o usuario esta correto ou nao
 
     if(random == 0)
     {
@@ -70,7 +69,7 @@ btn.addEventListener('click', ()=>{
         {
             console.log("REPOSTA CORRETA")
             aviso.innerHTML = "Parabens você acertou !"
-            setTimeout(()=>{
+            setTimeout(()=>{ // caso acerte a pagina ira reiniciar para mostrar novos valores
             location.reload()
             }, 2000)
         }else 
@@ -83,7 +82,7 @@ btn.addEventListener('click', ()=>{
             console.log("REPOSTA CORRETA")
             aviso.innerHTML = "Parabens você acertou !"
             setTimeout(()=>{
-            location.reload()
+            location.reload() // caso acerte a pagina ira reiniciar para mostrar novos valores
             }, 2000)
         }else 
         {
